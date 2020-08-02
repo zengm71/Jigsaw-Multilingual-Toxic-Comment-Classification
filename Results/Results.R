b441f280df33501a5760afe9d4ef39ca2d5dee1f
MichaelZeng <zengm71@berkeley.edu>
2020-08-01 22:01:04 -0700

    update the paper for final submission

--- Results/Results.R
+++ Results/Results.R
@@ -12,12 +12,15 @@ as.tbl(Results) %>% gather(key = 'key', value = 'value', `OLID 0-shot`:`OLID 0-s
   mutate(key = factor(key, 
                          levels = c('OLID 0-shot', 'OLID 0-shot 1k','OLID 0-shot 2k' ,
                                     'OLID 0-shot 5k', 'OLID 0-shot 10k'))) %>%
-  ggplot(aes(x = size, y = value, colour = Model, lty = Freeze,
-             group = g)) + geom_line() + 
+  ggplot() + geom_line(aes(x = size, y = value, colour = Model, lty = Freeze,
+                           group = g)) + 
+  geom_hline(aes(yintercept = .7, 
+                 colour = 'Zampieri et al., 2019'), show.legend = T) + 
   scale_x_continuous(breaks = c(0, 1000, 2000, 5000, 10000), labels = c('0-shot', '1k', '2k', '5k', '10k')) + 
   labs(x = '', y = 'F1 Score', lty = 'Freeze Transformer Weights', 
        title = 'Transfer Learning Performance on OLID') + 
-  theme_minimal() + theme(legend.position = 'bottom', legend.box="vertical", legend.margin=margin()) +
-  guides(colour=guide_legend(nrow=2,byrow=TRUE))
+  theme_minimal(base_size = 10) + 
+  theme(legend.position = 'bottom', legend.box="vertical", legend.margin=margin(), legend.text.align = 0) +
+  guides(colour=guide_legend(nrow=2,byrow=TRUE)) 
 
-ggsave(file = 'transfer_learning_performance_nominal.png', width = 4, height = 4, units = 'in')
+ggsave(file = 'transfer_learning_performance_nominal.png', width = 4.5, height = 4, units = 'in')
